e0c21fdb0ca2ec3a9764b9ba57b5a5980a902a03
kwahlberg <33008515+kwahlberg@users.noreply.github.com>
2017-11-22 06:17:36 -0800

    Update pathToURL.php

--- project_2/pathToURL.php
+++ project_2/pathToURL.php
@@ -4,33 +4,41 @@
     exit;
   }
 // diplays a single url as full url. takes relative path as arg.
-function displayURL($path) {
-        global $full_directory;
-        global $active_directory;
+function displayURL($path,$active_dir) {
+       // global $_SESSION["currentDir"];
         if(preg_match("/^[A-Za-z0-9_ ]+\.([A-Za-z0-9]+$)/", $path)){
-
-                $dir_split = preg_split("/\/public_html/", $full_directory);
+                $dir_split = preg_split("/\/public_html/", $_SESSION["currentDir"]);
                 //echo "$dir_split[0]";
-                $user = '/'.'~'. substr($dir_split[0], strrpos($dir_split[0], '/') + 1);;
+                $user = '/'.'~'. substr($dir_split[0], strrpos($dir_split[0], '/') + 1);
                 $full_path ='https://'. $_SERVER['SERVER_NAME'] . $user . $dir_split[1] . '/' . $path;
-                $type = mime_content_type ($active_directory .'/'. $path);
-
+                $type = mime_content_type ($active_dir .'/'. $path);
                 echo "<br><a href = '$full_path'>";
                 if(preg_match('/^image/',$type)){
-                        echo "<br><img src='$full_path' style='width:42px;height:42px;border:20px;'>'".$full_path."</a>";
+                        echo "<br><img src='$full_path' style='width:42px;height:42px;border:1;'>'".$full_path."</a>";
                 }else{
-                echo  $full_directory . "</a>";
+                echo  $full_path . "</a>";
                 }
                 //echo "<p>resource not found. Please check path and try again</p>";
         }
 }
-//displays list of urls takes array of strings
-function listURL($urlarray){
-        global $full_directory;
-        if(is_array($urlarray)){
-                foreach($urlarray as $value){
-                        //echo $full_directory . "/" . $value;
-                        displayURL($value);
+
+//take array or take dir path to create urls from array of file names as strings
+function listURL($path){
+        if(!is_array($path)){
+                //print_r($path);
+                $dir_split = preg_split("/\/public_html/", $path);
+                $afterdata = preg_split("/\/data/", $dir_split[1]);
+                $withdata = 'data' . $afterdata[1];
+                $karray = scandir($withdata);
+        }else{
+              	$karray = $path;
+        }
+
+	global $full_directory;
+        if(is_array($karray)){
+                foreach($karray as $value){
+                        //echo "<br>" . "/" . $value;
+                        displayURL($value, $withdata);
                 }
         }
 }
@@ -39,9 +47,6 @@ function listURL($urlarray){
 //$urls = ['week10.php','../test/test.php','week3.php'];
 //listURL($urls);
 ?>
-
-
-
 
 
 
