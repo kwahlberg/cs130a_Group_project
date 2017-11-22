8444bdd080a14d20afc2abc0f346e71510b61892
kwahlberg <33008515+kwahlberg@users.noreply.github.com>
2017-11-21 19:37:39 -0800

    Update pathToURL.php

--- file_manager/pathToURL.php
+++ file_manager/pathToURL.php
@@ -6,14 +6,22 @@
 // diplays a single url as full url. takes relative path as arg.
 function displayURL($path) {
         global $full_directory;
+        global $active_directory;
         if(preg_match("/^[A-Za-z0-9_ ]+\.([A-Za-z0-9]+$)/", $path)){
 
                 $dir_split = preg_split("/\/public_html/", $full_directory);
                 //echo "$dir_split[0]";
-                $user = '/'.'~'. substr($dir_split[0], strrpos($dir_split[0], '$
-                $full_path ='https://'. $_SERVER['SERVER_NAME'] . $user . $dir_$
-                echo "<p><a href = '$full_path'>" .$full_path . "</a></p>";
-                //echo "<p>resource not found. Please check path and try again<$
+                $user = '/'.'~'. substr($dir_split[0], strrpos($dir_split[0], '/') + 1);;
+                $full_path ='https://'. $_SERVER['SERVER_NAME'] . $user . $dir_split[1] . '/' . $path;
+                $type = mime_content_type ($active_directory .'/'. $path);
+
+                echo "<br><a href = '$full_path'>";
+                if(preg_match('/^image/',$type)){
+                        echo "<br><img src='$full_path' style='width:42px;height:42px;border:20px;'>'".$full_path."</a>";
+                }else{
+                echo  $full_directory . "</a>";
+                }
+                //echo "<p>resource not found. Please check path and try again</p>";
         }
 }
 //displays list of urls takes array of strings
@@ -31,6 +39,10 @@ function listURL($urlarray){
 //$urls = ['week10.php','../test/test.php','week3.php'];
 //listURL($urls);
 ?>
+
+
+
+
 
 
 
