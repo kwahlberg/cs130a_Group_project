aa0e2bca3ec59c73fe3532bda64954af561f8a0e
kwahlberg <33008515+kwahlberg@users.noreply.github.com>
2017-12-08 18:52:21 -0800

    Update home.php

--- project_3/SecurityLog/phplib/app/views/home.php
+++ project_3/SecurityLog/phplib/app/views/home.php
@@ -1,6 +1,22 @@
 <html>  
 <head></head>  
-  
+<style>
+table {
+    font-family: arial, sans-serif;
+    border-collapse: collapse;
+    width: 100%;
+}
+
+td, th {
+    border: 1px solid #dddddd;
+    text-align: left;
+    padding: 8px;
+}
+
+tr:nth-child(even) {
+    background-color: #dddddd;
+}
+</style>  
 <body>    
        
     <form action='LogModel.php?page="login"' autocomplete='on' method='POST'>
@@ -21,5 +37,15 @@ foreach($tenants as $tenant){
         <tr><th>ID</th><th>Visitor Name</th><th>Visiting</th><th>Time In</th><th>Time Out</th></tr>  
         
 <?php 
-    foreach(
+    foreach($model->visitors_in as $visitor){
+      
+      echo " <tr><td> $visitor['v_id']</td>";
+      echo " <td> $visitor['t_name']</td>";
+      echo " <td> $visitor['ts_in']</td>";
+      echo " <td> $visitor['ts_out']</td></tr>";
+    }
+?>
+      </table>
+      </body>
+  
     
