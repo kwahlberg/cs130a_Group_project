25dca182146efb2e52470c7a2136a868d1309923
eagarcia8 <eagarcia8@gmail.com>
2017-12-10 00:29:21 -0800

    added other views

---
+++ project_3/SecurityLog/phplib/app/views/history.php
@@ -0,0 +1,52 @@
+<!doctype html>
+
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+
+  <title>Security Log PHP App</title>
+  <meta name="description" content="Project for CCSF 130A">
+  <meta name="author" content="By Eduardo Garcia, Kurt Wahlberg, and Thi Mach">
+
+  <link rel="stylesheet" href="css/styles.css?v=1.0">
+
+  <!--[if lt IE 9]>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.js"></script>
+  <![endif]-->
+</head>
+
+<body>
+
+  <div id="current-visitors">
+    <h1>History Log</h1>
+
+    <table>
+      <tr>
+        <th>ID</th>
+        <th>Visitor Name</th>
+        <th>Visiting</th>
+        <th>Time In</th>
+        <th>Time Out</th>
+      </tr>
+      <tr>
+        <td>visitor </td>
+        <td>tenant name</td>
+        <td>time in</td>
+        <td>time out</td>
+      </tr>
+<?php
+foreach($model->visitors_in as $visitor){
+  echo "<tr>" .
+    "<td>$visitor['v_id']</td>" .
+    "<td>$visitor['t_name']</td>" .
+    "<td>$visitor['ts_in']</td>" .
+    "<td>'Currently Inside'</td>" .
+    "</tr>";
+  }
+?>
+    </table>
+  </div>
+
+  <script src="js/scripts.js"></script>
+</body>
+</html>
---
+++ project_3/SecurityLog/phplib/app/views/login.php
@@ -0,0 +1,27 @@
+<!doctype html>
+
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+
+  <title>Security Log PHP App</title>
+  <meta name="description" content="Project for CCSF 130A">
+  <meta name="author" content="By Eduardo Garcia, Kurt Wahlberg, and Thi Mach">
+
+  <link rel="stylesheet" href="css/styles.css?v=1.0">
+
+  <!--[if lt IE 9]>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.js"></script>
+  <![endif]-->
+</head>
+
+<body>
+
+  <div id="login-form">
+    <h1>You are now logged out!</h1>
+    <p>Click <a href="index.php">here</a> to return to the main page.</p>
+  </div>
+
+  <script src="js/scripts.js"></script>
+</body>
+</html>
