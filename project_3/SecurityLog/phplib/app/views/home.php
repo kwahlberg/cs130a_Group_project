f80f29d38ad767a0e6ff5b05de5edeb56a660313
eagarcia8 <eagarcia8@gmail.com>
2017-12-09 19:04:43 -0800

    updated home.php to current prototype

--- project_3/SecurityLog/phplib/app/views/home.php
+++ project_3/SecurityLog/phplib/app/views/home.php
@@ -17,24 +17,27 @@
 
 <body>
 
-    <div id="login-form">
+  <div id="login-form">
+    <h1>Register Your Visit!</h1>
 
     <form action='index.php?page="login"' autocomplete='on' method='POST'>
-      <p>Visitor name</p>
-      <input type='text' name='v_name' maxlength='30' required />
-      <p>Tenant name</p>
-      <input type='text' name='t_name' maxlength='30' required />
-      <select name="tenant">
+      <p class="label">Visitor Name</p>
+      <p><input type='text' name='v_name' maxlength='30' required /></p>
+      <p class="label">Tenant Name</p>
+      <p><input type='text' name='t_name' maxlength='30' required /></p>
+      <p><select name="tenant">
 <?php
 foreach($model->tenants as $tenant){
   echo '<option value="' . $tenant['t_name'] . '">' . $tenant['t_name'] . '</option>';
 }
 ?>
-      </select>
-      <input type="submit" value="Log Visit" />
+      </select><input type="submit" value="Log Visit" /></p>
     </form>
+  </div>
+
 
   <div id="current-visitors">
+    <h1>Current Visitors</h1>
 
     <table>
       <tr>
