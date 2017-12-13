6f41fbf359dccc307c2e22247cf047a2a2bff7d5
eagarcia8 <eagarcia8@gmail.com>
2017-12-12 17:52:27 -0800

    Fixed navbar CSS

--- project_3/SecurityLog/index.php
+++ project_3/SecurityLog/index.php
@@ -7,19 +7,28 @@
   <title>Security Log PHP App</title>
   <meta name="description" content="Project for CCSF 130A">
   <meta name="author" content="By Eduardo Garcia, Kurt Wahlberg, and Thi Mach">
-	<body>
-		<?php
-		//error_reporting(E_ALL); ini_set('display_errors', 1);
-			if (isset($_GET['source'])) {
-				highlight_file($_SERVER['SCRIPT_FILENAME']);
-				exit;
-			}  
-			include_once("phplib/app/controllers/Controller.php");  
 
-			$controller = new Controller();  
-			$controller->invoke();  
-		?>
-		
-	</body>
+  <link rel="stylesheet" href="css/styles.css?v=1.0">
+
+  <!--[if lt IE 9]>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.js"></script>
+  <![endif]-->
 </head>
 
+<body>
+
+  <?php
+  //error_reporting(E_ALL); ini_set('display_errors', 1);
+    if (isset($_GET['source'])) {
+      highlight_file($_SERVER['SCRIPT_FILENAME']);
+      exit;
+    }
+    include_once("phplib/app/controllers/Controller.php");
+
+    $controller = new Controller();
+    $controller->invoke();
+  ?>
+
+  <script src="js/scripts.js"></script>
+</body>
+</html>
--- project_3/SecurityLog/phplib/app/views/navbar.php
+++ project_3/SecurityLog/phplib/app/views/navbar.php
@@ -1,58 +1,71 @@
-<!DOCTYPE html>
-<html>
+<!doctype html>
+
+<html lang="en">
 <head>
-<style>
-ul {
-    list-style-type: none;
-    margin: 0;
-    padding: 0;
-    overflow: hidden;
-    background-color: #333;
-}
+  <meta charset="utf-8">
 
-li {
-    float: left;
-}
-    
-ri {
-    float: right;
-    list-style-type: none;
-    margin: 0;
-    padding: 0;
-    overflow: hidden;
-    background-color: #333;
-    display: block;
-    color: white;
-    text-align: center;
-    padding: 14px 16px;
-    text-decoration: none;
-}
+  <title>Security Log PHP App</title>
+  <meta name="description" content="Project for CCSF 130A">
+  <meta name="author" content="By Eduardo Garcia, Kurt Wahlberg, and Thi Mach">
 
-li a {
-    display: block;
-    color: white;
-    text-align: center;
-    padding: 14px 16px;
-    text-decoration: none;
-}
+  <style>
+  ul {
+      list-style-type: none;
+      margin: 0;
+      padding: 0;
+      overflow: hidden;
+      background-color: #333;
+  }
 
-li a:hover {
-    background-color: #111;
-}
-</style>
+  li {
+      float: left;
+  }
+
+  ri {
+      float: right;
+      list-style-type: none;
+      margin: 0;
+      padding: 0;
+      overflow: hidden;
+      background-color: #333;
+      display: block;
+      color: white;
+      text-align: center;
+      padding: 14px 16px;
+      text-decoration: none;
+  }
+
+  li a {
+      display: block;
+      color: white;
+      text-align: center;
+      padding: 14px 16px;
+      text-decoration: none;
+  }
+
+  li a:hover {
+      background-color: #111;
+  }
+  </style>
+
+  <link rel="stylesheet" href="css/styles.css?v=1.0">
+
+  <!--[if lt IE 9]>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.js"></script>
+  <![endif]-->
 </head>
+
 <body>
 
-<ul>
-  <li><a class="active" href="index.php?page=home">Home</a></li>
-  <li><a href="index.php?page=history">History</a></li>
-  <li><a href="index.php?page=average">Average Visit</a></li>
-  <li><a href="index.php?page=biglogout">Logout</a></li>
-<ri>NORTHWOODS SECURITY</ri>
-   
-</ul>
-    
+  <ul id="nav">
+    <li><a class="active" href="index.php?page=home">Home</a></li>
+    <li><a href="index.php?page=history">History</a></li>
+    <li><a href="index.php?page=average">Average Visit</a></li>
+    <li><a href="index.php?page=biglogout">Logout</a></li>
+  <ri>NORTHWOODS SECURITY</ri>
 
+  </ul>
+
+  <script src="js/scripts.js"></script>
 </body>
 </html>
-
